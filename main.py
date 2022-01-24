ddbfb072a84837d6acd805565bb8c8b05d9ee5a9
Dounas P <ntinospalegas@gmail.com>
2022-01-24 15:05:23 +0200

    αλλαγή της αρχικοποίησης του mincost από το δεύτερο for στο πρώτο και πρόσθεσα έλεγχο για τοπικό μέγιστο

--- main.py
+++ main.py
@@ -41,14 +41,13 @@ def gsat(maxTries,maxFlips):
 
     for j in range(maxTries): #max προσπάθειες
         binaryvalues = []  # μεταβλητή για να κρατάει boolean τιμές στις θέσεις τον λεκτικών
-
+        mincost = c  # μεταβλητή για να κρατάει το μικρότερο κόστος των κινήσεων
         for i in range(p):#αναθέτουμε random bool τιμές
             binaryvalues.append(bool(random.getrandbits(1)))
         print(binaryvalues)
         data=[]#πίνακας που κρατάει το αποτέλεσμα της πρότασης
 
         for k in range(maxFlips):
-            mincost=c   #μεταβλητή για να κρατάει το μικρότερο κόστος των κινήσεων
             tmpointer=[]     #μεταβλητή που έχει δείκτη/ες από για τις αλλαγές με το μικρότερο κόστος
             tmplist=[]       #λίστα για να κρατάει τα κόστη των κινήσεων
             data.append(data_structure(characters,kb,binaryvalues))
@@ -68,6 +67,9 @@ def gsat(maxTries,maxFlips):
                 if(tmplist[i] == mincost):
                     tmpointer.append(i)
             print("δείκτες",tmpointer)
+
+            if len(tmpointer) == 0:
+                break
 
             if(len(tmpointer)>1):
                 flip(binaryvalues,tmpointer[random.randint(0,len(tmpointer)-1)])
@@ -94,23 +96,24 @@ def gsat(maxTries,maxFlips):
 
 
 def main():
-    ckb.createKnowledgeBase(4, 20, 4)
+    ckb.createKnowledgeBase(3, 10, 3)
     p, c, l = ckb.retKBcharacteristics()
-    while(1):
-        newcharacter=input("dwse xaraktira re arxidi\n")
-        c=c+1
-        kb, characters = ckb.readKnowledgeBase()
-        kb.insert(0,newcharacter.swapcase())
-        if gsat(10,20):
-            knowledgeBaseFile = open('knowledgeBase.txt', 'a')
-            knowledgeBaseFile.write(newcharacter)
-            knowledgeBaseFile.close()
-        else:
-            c=c-1
-            kb.pop(0)
-        tmpchar=input("Shall we continue sir? (y/n)")
-        if tmpchar == "n":
-            break
+    gsat(10, 20)
+    # while(1):
+    #     newcharacter=input("dwse xaraktira re arxidi\n")
+    #     c=c+1
+    #     kb, characters = ckb.readKnowledgeBase()
+    #     kb.insert(0,newcharacter.swapcase())
+    #     if gsat(10,20):
+    #         knowledgeBaseFile = open('knowledgeBase.txt', 'a')
+    #         knowledgeBaseFile.write(newcharacter)
+    #         knowledgeBaseFile.close()
+    #     else:
+    #         c=c-1
+    #         kb.pop(0)
+    #     tmpchar=input("Shall we continue sir? (y/n)")
+    #     if tmpchar == "n":
+    #         break
 
 
 
